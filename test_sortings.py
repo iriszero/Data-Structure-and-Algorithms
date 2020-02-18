1951ad5ce2d50690e6182a5b751ca718e63727cf
Jaeseok Huh (Jason) <jaeseok.huh.0@gmail.com>
2020-02-18 11:36:44 +0900

    Add edge case testing

--- test_sortings.py
+++ test_sortings.py
@@ -4,13 +4,29 @@ import pytest
 from sortings import merge_sort, quick_sort
 
 @pytest.fixture()
-def random_array(k=100):
-    return random.sample(range(10000), k)
+def random_array(k=100000):
+    return random.sample(range(10000000), k)
+
+@pytest.fixture()
+def sorted_array(k=100000):
+    return [i for i in range(k)]
+
+@pytest.fixture()
+def duplicative_array(k
+=100000):
+    return [7] * k
 
 def test_merge_sort(random_array):
     n = len(random_array)
     assert(sorted(random_array) == merge_sort(random_array, n))
 
-def test_quick_sort(random_array):
+def test_quick_sort_random(random_array):
     n = len(random_array)
     assert(sorted(random_array) == quick_sort(random_array, n))
+
+def test_quick_sort_edges(sorted_array, duplicative_array):
+    n = len(sorted_array)
+    assert(sorted(sorted_array) == quick_sort(sorted_array, n))
+    
+    n = len(duplicative_array)
+    assert(sorted(duplicative_array) == quick_sort(duplicative_array, n))
